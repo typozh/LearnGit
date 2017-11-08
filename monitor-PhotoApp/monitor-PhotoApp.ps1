cf7898d07b6ceb189d50e31bd671f6d0614a9d31
Brian Douglas <brian@douglas.li>
2017-11-08 20:15:53 +0100

    starting the photo app if not running

--- monitor-PhotoApp/monitor-PhotoApp.ps1
+++ monitor-PhotoApp/monitor-PhotoApp.ps1
@@ -3,15 +3,23 @@ $WarningPreference = "Continue"
 $InformationPreference = "Continue"
 $PhotoAppProcessName = "Microsoft.Photos"
 $WaitSeconds = 10
-
+$StartPhotoAppCommand = "start ms-photos:"
 do {
     $PhotoAppProcess = get-process -Name $PhotoAppProcessName -ErrorAction SilentlyContinue
     if($PhotoAppProcess -eq $null) {
-        Write-Warning ("Photo App is not running")
+        Write-Warning ("Photo App is not running will try to start the app")
+        try {
+            Invoke-Expression -Command $StartPhotoAppCommand -ErrorAction Stop
+            Write-Information ("Photo App started successfully ;-) ")
+        }
+        catch {
+            Write-Error ("Photo App could not be started!")
+        }
     }
     else  {
         Write-Information ("Photo App is running")
     }
    Start-Sleep -Seconds $WaitSeconds
 } until ($false)
+
 
