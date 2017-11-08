34a5a4a6bec92b01935cbf169827a7719edd4b23
Brian Douglas <brian@douglas.li>
2017-11-08 20:06:23 +0100

    using write-information and write-warning instead of write-host

--- monitor-PhotoApp/monitor-PhotoApp.ps1
+++ monitor-PhotoApp/monitor-PhotoApp.ps1
@@ -1,14 +1,16 @@
 Clear-Host
-
+$WarningPreference = "Continue"
+$InformationPreference = "Continue"
 $PhotoAppProcessName = "Microsoft.Photos"
 $WaitSeconds = 10
+
 do {
     $PhotoAppProcess = get-process -Name $PhotoAppProcessName -ErrorAction SilentlyContinue
     if($PhotoAppProcess -eq $null) {
-        Write-Host ("Photo App is not running")
+        Write-Warning ("Photo App is not running")
     }
     else  {
-        Write-Host ("Photo App is running")
+        Write-Information ("Photo App is running")
     }
    Start-Sleep -Seconds $WaitSeconds
 } until ($false)
